ccfde9c499d6b72af1e8259a37d656e4ffca92d0
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-26 15:10:54 -0500

    Initialized a node express server and created an inial book model

---
+++ node-server/model/book.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const schema = mongoose.schema;
+
+let Book = new Schema(
+  {
+    name: {
+      type: String,
+    },
+    price: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    collection: "books",
+  }
+);
