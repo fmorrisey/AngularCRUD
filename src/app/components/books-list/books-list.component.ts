c6fdfe836138233858617261d1ef077bf6fe57cf
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-04-08 11:31:27 -0500

    Chased down a delete error

--- src/app/components/books-list/books-list.component.ts
+++ src/app/components/books-list/books-list.component.ts
@@ -20,7 +20,7 @@ export class BooksListComponent implements OnInit {
 
   delete(id: any, i: any) {
     console.log(id);
-    if (window.confirm('Do you wanna to delete this book?' + id)) {
+    if (window.confirm('Do you wanna to delete this book?')) {
       this.crudService.deleteBook(id).subscribe((res) => {
         this.Books.splice(i, 1);
       });
